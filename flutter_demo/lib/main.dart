1696b8db5e59b95f6a16c05dc8a199aeea322987
okboy <okboy_2008@msn.com>
2024-09-23 14:23:28 +0800

    remove bigcard

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -138,39 +138,3 @@ class FavoritePage extends StatelessWidget {
     );
   }
 }
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Wrap(
-          children: [
-            Text(
-                  pair.first,
-                  style: style.copyWith(fontWeight: FontWeight.w200),
-                ),
-            Text(
-                  pair.second,
-                  style: style.copyWith(fontWeight: FontWeight.bold),
-                ),
-            ]
-          ),
-      ),
-    );
-  }
-}
