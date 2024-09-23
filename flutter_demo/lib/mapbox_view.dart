429943aaeb81e59be55624fbe7778b81d3cf0733
Wuying Created Local Users <okboy_2008@s8sqrmwwj20pxjj.CN-SHANGHAI-G-198-1.WUYING.LOCAL>
2024-09-23 11:47:19 +0800

    add search box on top left

--- flutter_demo/lib/mapbox_view.dart
+++ flutter_demo/lib/mapbox_view.dart
@@ -16,14 +16,43 @@ class _GeneratorPageState extends State<GeneratorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapboxMap(
-        accessToken: 'pk.eyJ1Ijoib2tib3kyMDA4IiwiYSI6ImNsdGE1dzd6OTAxbHQyanA0aWM1MjU5c24ifQ.vbbY3gzL8nnUFctmDv9UBQ', // Replace with your Mapbox access token
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(45.009487, 6.124711), // Coordinates for les 2 alps
-          zoom: 13,
-        ),
-        styleString: 'mapbox://styles/okboy2008/clx1zai3s01ck01rb5zsv600u',
+      body: Stack(
+        children: [
+          MapboxMap(
+            accessToken:
+                'pk.eyJ1Ijoib2tib3kyMDA4IiwiYSI6ImNsdGE1dzd6OTAxbHQyanA0aWM1MjU5c24ifQ.vbbY3gzL8nnUFctmDv9UBQ',
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(45.009487, 6.124711), // Coordinates for les 2 alps
+              zoom: 13,
+            ),
+            styleString: 'mapbox://styles/okboy2008/clx1zai3s01ck01rb5zsv600u',
+          ),
+          Positioned(
+            top: 20,  // Position at the top
+            left: 20, // Position at the left
+            child: Container(
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),  // Set opacity
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search location',
+                  prefixIcon: Icon(Icons.search),  // Add search icon
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.6),  // Set the fill color with opacity
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
