350d752f393795a897a0885fdf338f683edac9a4
okboy <okboy_2008@msn.com>
2024-09-24 12:23:50 +0800

    add arrow on line

--- lib/mapbox_view.dart
+++ lib/mapbox_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 class GeneratorPage extends StatefulWidget {
   @override
@@ -10,6 +12,45 @@ class GeneratorPage extends StatefulWidget {
 
 class _GeneratorPageState extends State<GeneratorPage> {
   MapboxMapController? mapController;
+  // Function to create a Flutter icon as an image (in memory) that takes the icon as a parameter
+  Future<Uint8List> _createFlutterIconAsImage(IconData iconData, Color color, double size) async {
+    final recorder = ui.PictureRecorder();
+    final canvas = Canvas(recorder);
+    final textPainter = TextPainter(textDirection: TextDirection.ltr);
+
+    // Create the Flutter icon widget as a picture
+    textPainter.text = TextSpan(
+      text: String.fromCharCode(iconData.codePoint),
+      style: TextStyle(
+        fontSize: size,
+        fontFamily: iconData.fontFamily,
+        color: color,
+      ),
+    );
+    textPainter.layout();
+    textPainter.paint(canvas, Offset(0.0, 0.0));
+
+    // Convert the picture to an image
+    final picture = recorder.endRecording();
+    final img = await picture.toImage(size.toInt(), size.toInt());
+    final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
+  }
+
+  // Add Flutter icon as an image to Mapbox
+  Future<void> _addFlutterIconToMap({
+    required IconData icon,
+    required Color color,
+    required double size,
+    required String imageName,
+  }) async {
+    // Create an image from the Flutter icon
+    Uint8List iconImage = await _createFlutterIconAsImage(icon, color, size);
+
+    // Add the image to the Mapbox map
+    mapController?.addImage(imageName, iconImage);
+  }
+
   String? geojsonData;
 
   // Function to load GeoJSON from assets
@@ -35,12 +76,12 @@ class _GeneratorPageState extends State<GeneratorPage> {
       }).toList(),
     };
 
-    // Separate piste:type features
+    // Separate piste:type features and filter out those that have the 'area' key
     final pisteFeatures = {
       "type": "FeatureCollection",
       "features": (parsedGeoJson['features'] as List).where((feature) {
         return feature['properties'].containsKey('piste:type') &&
-        !feature['properties'].containsKey('area'); // Filter out areas;
+               !feature['properties'].containsKey('area'); // Filter out areas
       }).toList(),
     };
 
@@ -77,6 +118,34 @@ class _GeneratorPageState extends State<GeneratorPage> {
       ),
     );
 
+    // Add arrows for aerialway (one arrow per line)
+    mapController?.addSymbolLayer(
+      'aerialway-source',
+      'aerialway-arrow-layer',
+      SymbolLayerProperties(
+        iconImage: "lift-arrow", // Built-in arrow icon
+        symbolPlacement: 'line-center', // Place along the line
+        symbolSpacing: 5000000, // Ensures only one arrow is placed on the line
+        iconAllowOverlap: true,
+        iconRotate: ['get', 'bearing'], // Rotate arrow based on line bearing
+        iconRotationAlignment: 'map',
+      ),
+    );
+
+    // Add arrows for piste:type (one arrow per line)
+    mapController?.addSymbolLayer(
+      'piste-source',
+      'piste-arrow-layer',
+      SymbolLayerProperties(
+        iconImage: "lift-arrow", // Built-in arrow icon
+        symbolPlacement: 'line', // Place along the line
+        symbolSpacing: 100, // Ensures only one arrow is placed on the line
+        iconAllowOverlap: true,
+        iconRotate: ['get', 'bearing'], // Rotate arrow based on line bearing
+        iconRotationAlignment: 'map',
+      ),
+    );
+
     print('Layers for aerialway and piste added successfully');
   }
 
@@ -102,7 +171,12 @@ class _GeneratorPageState extends State<GeneratorPage> {
   }
 
   void _onStyleLoadedCallback() async {
-    // _addPistesLayer();
+    _addFlutterIconToMap(
+      icon: Icons.arrow_right,
+      color: const Color.fromARGB(255, 0, 0, 0),
+      size: 25.0,
+      imageName: 'lift-arrow',
+    );
     _loadGeoJsonFromAssets();
   }
 
